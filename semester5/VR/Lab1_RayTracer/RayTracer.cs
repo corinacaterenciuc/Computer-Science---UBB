18b7c630752f6d674a49268ff10baf5fb5253770
corinacaterenciuc <corina.a.caterenciuc@gmail.com>
2021-02-06 12:47:26 +0200

    Ray-Tracer lab

---
+++ semester5/VR/Lab1_RayTracer/RayTracer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace rt
+{
+    class RayTracer
+    {
+        private Geometry[] geometries;
+        private Light[] lights;
+
+        public RayTracer(Geometry[] geometries, Light[] lights)
+        {
+            this.geometries = geometries;
+            this.lights = lights;
+        }
+
+        private double ImageToViewPlane(int n, int imgSize, double viewPlaneSize)
+        {
+            var u = n * viewPlaneSize / imgSize;
+            u -= viewPlaneSize / 2;
+            return u;
+        }
+
+        private Intersection FindFirstIntersection(Line ray, double minDist, double maxDist)
+        {
+            var intersection = new Intersection();
+
+            foreach (var geometry in geometries)
+            {
+                var intr = geometry.GetIntersection(ray, minDist, maxDist);
+
+                if (!intr.Valid || !intr.Visible) continue;
+
+                if (!intersection.Valid || !intersection.Visible)
+                {
+                    intersection = intr;
+                }
+                else if (intr.T < intersection.T)
+                {
+                    intersection = intr;
+                }
+            }
+
+            return intersection;
+        }
+
+        private bool IsLit(Vector point, Light light)
+        {
+            // ADD CODE HERE: Detect whether the given point has a clear line of sight to the given light
+
+            Line ray = new Line(point, light.Position);
+            double d = (light.Position - point).Length();
+            Intersection intersect = FindFirstIntersection(ray, 0, d);
+
+            return !(intersect.Valid); //&& intersect.Visible);
+        }
+
+        public void Render(Camera camera, int width, int height, string filename)
+        {
+            var background = new Color(0,0,0,0); 
+            var viewParallel = (camera.Up ^ camera.Direction).Normalize();
+
+            var image = new Image(width, height);
+
+            var vecW = camera.Direction * camera.ViewPlaneDistance;
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    // ADD CODE HERE: Implement pixel color calculation
+
+                    Vector x1 = camera.Position + camera.Direction * camera.ViewPlaneDistance + viewParallel * ImageToViewPlane(i, width, camera.ViewPlaneWidth) + camera.Up * ImageToViewPlane(j, height, camera.ViewPlaneHeight);
+                    Line ray = new Line(camera.Position, x1);
+                    Intersection intersect = FindFirstIntersection(ray, camera.FrontPlaneDistance, camera.BackPlaneDistance);
+
+                    if(intersect.Valid && intersect.Visible)
+                    {
+                        Color color = new Color();
+                        //color += intersect.Geometry.Color;
+
+                        
+                        foreach (var light in lights)
+                        {
+                            color += intersect.Geometry.Material.Ambient * light.Ambient;
+                            bool isLit = IsLit(intersect.Position, light);
+
+                            if(isLit)
+                            {
+                                Vector N = intersect.Geometry.Normal(intersect.Position);
+                                Vector T = (light.Position - intersect.Position).Normalize();
+                                Vector E = (camera.Position - intersect.Position).Normalize();
+                                Vector R = N * (N * T) * 2 - T;
+
+                                if(N * T > 0)
+                                {
+                                    color += intersect.Geometry.Material.Diffuse * light.Diffuse * (N * T);
+                                }
+                                if(E * R > 0)
+                                {
+                                    color += intersect.Geometry.Material.Specular * light.Specular * Math.Pow(E * R, intersect.Geometry.Material.Shininess);
+                                }
+                                color *= light.Intensity;
+                            }
+                        }
+
+                        image.SetPixel(i, j, color);
+                    }
+                    else
+                        image.SetPixel(i, j, background);
+                }
+            }
+
+            image.Store(filename);
+        }
+    }
+}
---
+++ semester5/VR/Lab1_RayTracer/Sphere.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace rt
+{
+    public class Sphere : Geometry
+    {
+        private Vector Center { get; set; }
+        private double Radius { get; set; }
+
+        public Sphere(Vector center, double radius, Material material, Color color) : base(material, color)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public override Intersection GetIntersection(Line line, double minDist, double maxDist)
+        {
+            // ADD CODE HERE: Calculate the intersection between the given line and this sphere
+            double A = line.Dx.X * line.Dx.X + line.Dx.Y * line.Dx.Y + line.Dx.Z * line.Dx.Z;
+            double B = 2 * (line.Dx.X * line.X0.X - line.Dx.X * Center.X + line.Dx.Y * line.X0.Y - line.Dx.Y * Center.Y + line.Dx.Z * line.X0.Z - line.Dx.Z * Center.Z);
+
+            double x0xc = (line.X0.X - Center.X) * (line.X0.X - Center.X);
+            double y0yc = (line.X0.Y - Center.Y) * (line.X0.Y - Center.Y);
+            double z0zc = (line.X0.Z - Center.Z) * (line.X0.Z - Center.Z);
+
+            double C = x0xc + y0yc + z0zc - Radius * Radius;
+
+            double Delta = B * B - 4 * A * C;
+
+            double t1, t2;
+
+            if(Delta < 0)
+            {
+                return new Intersection();
+            }
+            else if(Delta == 0)
+            {
+                double t = -B / (2 * A);
+                Vector intersection = line.CoordinateToPosition(t);
+
+                bool visible = true;
+
+                double len = (intersection - line.X0).Length();
+
+                if (len < minDist || len > maxDist || t < 0)
+                    visible = false;
+
+                return new Intersection(true, visible, this, line, t);
+
+            }
+            else 
+            {
+                t1 = (-B + Math.Sqrt(Delta)) / (2 * A);
+                t2 = (-B - Math.Sqrt(Delta)) / (2 * A);
+                bool visible = true;
+
+                if(t1 < t2)
+                {
+                    Vector intersection = line.CoordinateToPosition(t1);
+
+                    double len = (intersection - line.X0).Length();
+                    if (len < minDist || len > maxDist || t1 < 0)
+                        visible = false;
+
+                    return new Intersection(true, visible, this, line, t1);
+                }
+                else
+                {
+                    Vector intersection = line.CoordinateToPosition(t2);
+
+                    double len = (intersection - line.X0).Length();
+                    if (len < minDist || len > maxDist || t2 < 0)
+                        visible = false;
+
+                    return new Intersection(true, visible, this, line, t2);
+                }
+
+            }
+
+            //return new Intersection();
+        }
+
+        public override Vector Normal(Vector v)
+        {
+            var n = v - Center;
+            n.Normalize();
+            return n;
+        }
+    }
+}
