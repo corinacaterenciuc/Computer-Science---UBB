ea28ac4007a51976cfed37795474b44ff56c82ae
corinacaterenciuc <corina.a.caterenciuc@gmail.com>
2021-03-08 13:35:49 +0200

    lab2 ssvv

---
+++ semester6/SSVV/FirstProjectBBT/src/test/java/ro/ubb/CCA/AppTest.java
@@ -0,0 +1,26 @@
+package ro.ubb.CCA;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test1()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void test2()
+    {
+        assertTrue( true );
+    }
+}
